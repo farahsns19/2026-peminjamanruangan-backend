3f5d906867d6c8216c80bf77cb2d0b20f4514bec
Farah H <macriv@Farah-H.local>
2026-02-16 12:14:06 +0700

    feat(api): finalize peminjaman crud

--- RoomBooking.API/Program.cs
+++ RoomBooking.API/Program.cs
@@ -6,15 +6,28 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services
 builder.Services.AddControllers();
 
-// Database SQLite
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=RoomBooking.db"));
-
 // Swagger services
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Database SQLite
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=RoomBooking.db"));
+
+
+//CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") // React Vite
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
 
 // Swagger middleware
 if (app.Environment.IsDevelopment())
@@ -23,7 +36,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+
+//Mengaktifkan CORS
+app.UseCors("AllowFrontend");
+
+app.UseAuthorization();
 
 app.MapControllers();
 
